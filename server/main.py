53313f095ea450347b6e2c302b9e2a536d446a05
Abhinandan Jain <nandanjain04@gmail.com>
2025-06-11 22:31:17 +0530

    Added server

--- server/main.py
+++ server/main.py
@@ -1,18 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from executor import run_compiler
-import tempfile
+import executor
 
 app = FastAPI()
 
-class CodeInput(BaseModel):
-    code: str = ""
-
-@app.post("/execute")
-async def execute_code(file: CodeInput) -> dict:
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".ffo") as temporary:
-        temporary.write(file.code.encode("utf-8"))
-        temporary_path = temporary.name
-
-    result = run_compiler(temporary_path)
-    return result
+app.include_router(executor.router)
