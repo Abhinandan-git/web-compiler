04e1d0ab82aa763605be76cde80097988dc52e60
Abhinandan Jain <nandanjain04@gmail.com>
2025-06-15 17:07:33 +0530

    form complete

--- server/executor.py
+++ server/executor.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 import subprocess, tempfile, os
 
 router = APIRouter(tags=["code-runner"])
 
+class CodeRequest(BaseModel):
+	source_code: str = ""
+
 @router.post("/run")
-def execute_code(source_code: str = "") -> dict:
+async def execute_code(request: CodeRequest) -> dict:
+	code = request.source_code
+
 	try:
 		with tempfile.NamedTemporaryFile(suffix=".ffo", mode="w+", delete=False) as temporary_file:
-			temporary_file.write(source_code)
+			temporary_file.write(code)
 			temporary_file_path = temporary_file.name
 
 		compile_result = subprocess.run(["./sacompiler", temporary_file_path], capture_output=True, text=True, timeout=5)
--- server/main.py
+++ server/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import executor
 
 app = FastAPI()
+
+origins = ['http://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
 
 app.include_router(executor.router)
